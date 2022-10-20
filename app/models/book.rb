74000d91a807390aa85ed92a3fdc5df3f4fe4172
Takako <takakoyamaguchi.0204@gmail.com>
2022-10-20 11:04:43 +0900

    本の画像用uploader作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+    mount_uploader :image, ImageUploader
+
+    validates :title, presence: true
+    validates :product_condition, presence: true
+    validates :price, presence: true
+    validates :postage, presence: true
 end
