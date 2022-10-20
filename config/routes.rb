5b1a0852cd2fb90bf8b379a8b1eb4f0a0b808a4b
Takako <takakoyamaguchi.0204@gmail.com>
2022-10-20 11:55:31 +0900

    コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/destroy'
+  get 'books/index'
+  get 'books/new'
+  get 'books/create'
+  get 'books/show'
+  get 'books/edit'
+  get 'books/update'
+  get 'books/destroy'
+  get 'users/index'
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   get 'hello/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "hello#index"
