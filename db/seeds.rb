1654e798d693966b4a4660a8ea9d15602e5e1565
Takako <takakoyamaguchi.0204@gmail.com>
2022-10-30 15:37:00 +0900

    カテゴリー項目

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create([
+    { name: '総記' },{ name: '哲学' },{ name: '歴史' },{ name: '社会科学' },{ name: '自然科学' },
+    { name: '技術' },{ name: '産業' },{ name: '芸術' },{ name: '言語' },{ name: '文学' },
+    { name: 'スポーツ' },{ name: '料理' },{ name: '旅行' },{ name: 'ビジネス書' },{ name: '小説、エッセイ' },
+    { name: '漫画、コミック' },{ name: '辞書' },{ name: '趣味、実用' },{ name: '専門書' },{ name: '参考書' },
+    { name: '絵本、児童書' },{ name: '雑誌' },{ name: '動物、植物' },{ name: '楽譜' },{ name: 'その他' }
+])
