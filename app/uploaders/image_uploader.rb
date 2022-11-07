0ad030a3fe1f3334e1880be3115d4d405376dffc
Takako <takakoyamaguchi.0204@gmail.com>
2022-11-07 16:03:15 +0900

    インデント直しました

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,7 +19,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   "no_image.jpg"
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
- end
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
