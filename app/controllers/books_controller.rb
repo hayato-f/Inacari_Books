ff273f1d918c167179ecf570210d84e655e468a1
Takako <takakoyamaguchi.0204@gmail.com>
2022-11-11 18:18:23 +0900

    編集ページに出品者以外入れない

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,6 +27,9 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    unless current_user == @book.seller
+      redirect_to root_path
+    end
   end
 
   def update
